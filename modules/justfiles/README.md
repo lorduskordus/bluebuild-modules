b0e4e7cd0b17c95d0e8eb5119b6bff58a0a61839
lorduskordus <lorduskordus@gmail.com>
2024-06-12 14:33:37 +0200

    Support validation in 'justfiles' module

--- modules/justfiles/README.md
+++ modules/justfiles/README.md
@@ -11,6 +11,8 @@ The `justfiles` module allows for easy just files importing.
 2. It finds all `.just` files inside of the `config/justfiles` folder or starting from the specified `include`.
     * If no `.just` files are found, the module fails.
 
+    * Optionally, it can also validate the justfiles.
+
     * The structure inside of that folder doesn't matter, you can place folders/files in there however you want, the module will find your `.just` files.
 
 3. It copies over your files/folders to `/usr/share/bluebuild/justfiles`.
@@ -25,10 +27,13 @@ Place all your `.just` files or folders with `.just` files inside the `config/ju
 
 Without specifying `include`, it will assume you want to import everything. Otherwise, specify your files/folders under `include`.
 
+If you want to validate your justfiles, set `validate: true`.
+
 ### Example Configuration
 
 ```yaml
 type: justfiles
+validate: true
 include:
     - gnome_folder/monitors.just
     - justfiles_folder
--- modules/justfiles/justfiles.sh
+++ modules/justfiles/justfiles.sh
@@ -23,6 +23,8 @@ fi
 # For every folder/file user specified, do
 for selected in "${CONFIG_SELECTION[@]}"; do
 
+    echo "---------- Adding folder/file '${selected}' ---------"
+
     # Find all justfiles, starting from 'selected' and get their paths
     JUSTFILES=($(find "${CONFIG_FOLDER}/${selected}" -type f -name "*.just" | sed "s|${CONFIG_FOLDER}/||g"))
 
@@ -30,33 +32,53 @@ for selected in "${CONFIG_SELECTION[@]}"; do
     if [[ ${#JUSTFILES[@]} == 0 ]]; then
         echo "Error: No justfiles were found in '${CONFIG_FOLDER}/${selected}'."
         exit 1
-    else
-        echo "---------- Adding folder/file '${selected}' ---------"
-
-        # Copy 'selected' to destination folder
-        echo "-- Copying folders/files"
-        mkdir -p "${DEST_FOLDER}/$(dirname ${selected})"
-        cp -rfT "${CONFIG_FOLDER}/${selected}" "${DEST_FOLDER}/${selected}"
-        echo "Copied '${CONFIG_FOLDER}/${selected}' to '${DEST_FOLDER}/${selected}'."
-
-        # Generate import lines for all found justfiles
-        echo "-- Adding import lines"
-        for justfile in "${JUSTFILES[@]}"; do
-
-            # Create an import line
-            IMPORT_LINE="import \"${DEST_FOLDER}/${justfile}\""
-            
-            # Abort if import line already exists, else append it to import file
-            if grep -wq "${IMPORT_LINE}" "${IMPORT_FILE}"; then
-                echo "Error: Duplicated import line: '${IMPORT_LINE}' found."
-                exit 1
-            else
-                echo "${IMPORT_LINE}" | tee -a "${IMPORT_FILE}"
-            fi
-
-        done
-        
-        echo "------------------------------------------------"
     fi
+
+    # Validate all found justfiles if set to do so
+    if [ "$(echo "$1" | yq -I=0 ".validate")" == "true" ]; then
+
+        echo "-- Validating justfiles"
+        VALIDATION_FAILED=0
+        for justfile in "${JUSTFILES[@]}"; do
+            if ! /usr/bin/just --fmt --check --unstable --justfile "${CONFIG_FOLDER}/${justfile}" &> /dev/null; then
+                echo "The justfile '${justfile}' FAILED validation."
+                VALIDATION_FAILED=1
+            fi
+        done
+
+        # Exit if any justfiles are not valid
+        if [ ${VALIDATION_FAILED} -eq 1 ]; then
+            echo "Error: Some justfiles didn't pass validation."
+            exit 1
+        else
+            echo "All justfiles passed validation."
+        fi
+
+    fi
+
+    # Copy 'selected' to destination folder
+    echo "-- Copying folders/files"
+    mkdir -p "${DEST_FOLDER}/$(dirname ${selected})"
+    cp -rfT "${CONFIG_FOLDER}/${selected}" "${DEST_FOLDER}/${selected}"
+    echo "Copied '${CONFIG_FOLDER}/${selected}' to '${DEST_FOLDER}/${selected}'."
+
+    # Generate import lines for all found justfiles
+    echo "-- Adding import lines"
+    for justfile in "${JUSTFILES[@]}"; do
+
+        # Create an import line
+        IMPORT_LINE="import \"${DEST_FOLDER}/${justfile}\""
+        
+        # Abort if import line already exists, else append it to import file
+        if grep -wq "${IMPORT_LINE}" "${IMPORT_FILE}"; then
+            echo "Error: Duplicated import line: '${IMPORT_LINE}' found."
+            exit 1
+        else
+            echo "${IMPORT_LINE}" | tee -a "${IMPORT_FILE}"
+        fi
+
+    done
+    
+    echo "------------------------------------------------"
 
 done
