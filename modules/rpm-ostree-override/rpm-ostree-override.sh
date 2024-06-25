72401ef299700d36c8bc4ab7b4fd7d9b784a8b45
lorduskordus <lorduskordus@gmail.com>
2024-06-25 10:34:41 +0200

    update 'rpm-ostree-override' module

--- modules/rpm-ostree-override/rpm-ostree-override.sh
+++ modules/rpm-ostree-override/rpm-ostree-override.sh
@@ -13,30 +13,31 @@ fi
 
 for replacement in "${REPLACEMENTS[@]}"; do
 
-    # Get Repository
+    # Get repository
     REPO=$(echo "${replacement}" | yq -I=0 ".from-repo")
     # Replace '%OS_VERSION%' with the current Fedora version
     REPO=${REPO//%OS_VERSION%/${OS_VERSION}}
+
+    # Ensure repository is provided
+    if [[ "${REPO}" == "null" ]]; then
+        echo "Error: Key 'from-repo' was declared, but repository URL was not provided."
+        exit 1
+    fi
+
+    # Get info from repository URL
+    MAINTAINER=$(awk -F'/' '{print $5}' <<< "${REPO}")
+    REPO_NAME=$(awk -F'/' '{print $6}' <<< "${REPO}")
+    FILE_NAME=$(awk -F'/' '{print $9}' <<< "${REPO}")
 
     # Get packages to replace
     get_yaml_array PACKAGES '.packages[]' "${replacement}"
     PACKAGES=$(echo "${PACKAGES[*]}" | tr -d '\n')
 
-    # Ensure repository is provided
-    if [[ "${REPO}" == "null" ]]; then
-        echo "Error: Repository was not provided."
-        exit 1
-    fi
-
     # Ensure packages are provided
     if [ -z "${PACKAGES}" ]; then
-        echo "Error: No packages were provided."
+        echo "Error: No packages were provided for repository '${REPO_NAME}'."
         exit 1
     fi
-
-    MAINTAINER=$(awk -F'/' '{print $5}' <<< "${REPO}")
-    REPO_NAME=$(awk -F'/' '{print $6}' <<< "${REPO}")
-    FILE_NAME=$(awk -F'/' '{print $9}' <<< "${REPO}")
 
     echo "-----------------------------------------------------------------------------------------------------"
     echo "------------ Replacing packages from repository: '${REPO_NAME}' owned by '${MAINTAINER}' ------------"
